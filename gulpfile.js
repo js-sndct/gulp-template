84109f9ad042eb06fba77e9840e0e2f147beb01a
syndicate <56651043+js-sndct@users.noreply.github.com>
2023-05-18 09:34:28 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@
 const {src, dest} = require("gulp")
 const gulp = require("gulp")
 const autoprefixer = require("gulp-autoprefixer")
-const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require('gulp-strip-css-comments');
-const rename = require("gulp-rename");
+const cssbeautify = require("gulp-cssbeautify")
+const removeComments = require('gulp-strip-css-comments')
+const rename = require("gulp-rename")
 const rigger = require("gulp-rigger")
-const sass = require("gulp-sass")(require('sass'));
-const cssnano = require("gulp-cssnano");
-const uglify = require("gulp-uglify");
-const plumber = require("gulp-plumber");
-const panini = require("panini");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
+const sass = require("gulp-sass")(require('sass'))
+const cssnano = require("gulp-cssnano")
+const uglify = require("gulp-uglify")
+const plumber = require("gulp-plumber")
+const panini = require("panini")
+const imagemin = require("gulp-imagemin")
+const del = require("del")
 const notify = require("gulp-notify")
 const imagewebp = require("gulp-webp")
-const browserSync = require("browser-sync").create();
+const browserSync = require("browser-sync").create()
 const copy = require("gulp-copy")
 
 /* Paths */
